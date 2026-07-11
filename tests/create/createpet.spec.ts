ce4a6f0c6306475b71716c64f1eff4ce8e1f0767
dmicu1 <danielmicu26@gmail.com>
2026-07-11 12:02:19 +0200

    added new public test

--- tests/create/createpet.spec.ts
+++ tests/create/createpet.spec.ts
@@ -1,4 +1,4 @@
-import test from "@playwright/test"
+import test, { expect } from "@playwright/test"
 import type { NewPetRequestModel } from "../../models/newPetRequestModel"
 import { CheckPetWasCreated } from "../../tasks/create/checkPetWasCreated"
 import { CreateNewPet } from "../../tasks/create/createNewPet"
@@ -51,18 +51,48 @@ test('Should create a new pet 2', async ({ request }) => {
 
 })
 
+test('Should create a new pet - public',{tag:'@prod'}, async ({ request }) => {
+  const createPetResponse = await request.post('https://petstore.swagger.io/v2/pet', {
+    data: {
+     
+  "id": 0,
+  "category": {
+    "id": 0,
+    "name": "string"
+  },
+  "name": "doggie",
+  "photoUrls": [
+    "string"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "string"
+    }
+  ],
+  "status": "available"
+}
+})
+expect(createPetResponse.status()).toBe(200)
+const createPetJsonResponse = await createPetResponse.json()
+expect(createPetJsonResponse.id).toBeTruthy()
+expect(createPetJsonResponse.name).toBe("doggie")
+expect(createPetJsonResponse.status).toBe("available")
+
+})
+
+
+
 /*test.afterAll("Despues todas", async()=>{
 
    Logger.info("Running after all")
 })
 
-/*test.afterEach(async () => {
+test.afterEach(async () => {
 
      Logger.info("running after test")
 
 })
 function async(arg0: { request: { (options: RequestOptions | string | URL, callback?: (res: IncomingMessage) => void): ClientRequest; (url: string | URL, options: RequestOptions, callback?: (res: IncomingMessage) => void): ClientRequest } }): (args: import("playwright/test").PlaywrightTestArgs & import("playwright/test").PlaywrightTestOptions & import("playwright/test").PlaywrightWorkerArgs & import("playwright/test").PlaywrightWorkerOptions, testInfo: import("playwright/test").TestInfo) => Promise<any> | any {
   throw new Error("Function not implemented.")
-}
-
-*/
+}*/
